cdfeee2e27fbd4ee89ae87a5882a51fbdf06a33b
Lucy Yu <lucyyu24@users.noreply.github.com>
2015-09-17 10:08:29 -0400

    Update September_17.sh

--- September_17.sh
+++ September_17.sh
@@ -4,52 +4,52 @@
 
 ls *.txt 			# see text files in current directory
 *.txt 				# is a glob pattern 
-example*.txt 		# wildcard can be inserted anywhere
+example*.txt 			# wildcard can be inserted anywhere
 ?.txt 				# matches one character
-ls badname* 		# > ls: badname*: No such file or directory
+ls badname* 			# > ls: badname*: No such file or directory
 
-echo '*.txt' 		# both quotes supress expansion of globs (see Quoting)
-echo "*.txt"		# literally print *.txt
+echo '*.txt' 			# both quotes supress expansion of globs (see Quoting)
+echo "*.txt"			# literally print *.txt
 
 # Shell Variables ---------------------------------------
 
 COURSE_NAME			# variable
-COURSE_NAME=CS246 	# variables defined using = (NO spaces!)
+COURSE_NAME=CS246 		# variables defined using = (NO spaces!)
 
-echo $COURSE_NAME	# retrieve variable value using $
-${COURSE_NAME}		# good practice 
+echo $COURSE_NAME		# retrieve variable value using $
+${COURSE_NAME}			# good practice 
 
 dir1=~/Teaching 
-echo ${dir1}		# > /Users/lucy/Teaching
+echo ${dir1}			# > /Users/lucy/Teaching
 
 first_nm=lucy 
-echo ${first_nm}yu 	# > lucyyyu
+echo ${first_nm}yu 		# > lucyyyu
 
 PATH				# several global variables are set up for you 
 CLASSPATH			# java
-0					# currently running script 
+0				# currently running script 
 
 # Quoting ----------------------------------------------
 
-echo '${PATH}'		# protect eveyrthing (even newline) except single quote
-					# > ${PATH}
+echo '${PATH}'			# protect eveyrthing (even newline) except single quote
+				# > ${PATH}
 
-echo "${PATH}"		# protect everything except double quote, backquote, and dollar sign
-					# will output the same as echo ${PATH}
+echo "${PATH}"			# protect everything except double quote, backquote, and dollar sign
+				# will output the same as echo ${PATH}
 
 # Working with Files, I/O Redirection --------------------
 
 cat 				# displays the contents of a file
-					# if no parameters are specified, cat waits for input from standard input
-					# CTRL-D is EOF
+				# if no parameters are specified, cat waits for input from standard input
+				# CTRL-D is EOF
 
 # in general, command args > file
 
-cat > output.txt 	# standard input will write to example.txt until EOF
+cat > output.txt 		# standard input will write to example.txt until EOF
 
-cat < input.txt 	# input can also be redirected
-					# shows contents of input.txt
-cat input.txt 		# different
+cat < input.txt 		# input can also be redirected
+				# shows contents of input.txt
+cat input.txt 			# different
 
 cat < myfile > myfile_redirected
 
@@ -57,7 +57,7 @@ cat < myfile > myfile_redirected
 
 # every process is attached to 3 streams
 # 	stdin -> program -> stderr (2)
-# 				   	 -> stout  (1)
+# 			 -> stout  (1)
 
 # stderr vs. stdout
 # 	separate output stream for error messages; avoid cluttering output files/corrupting formatting
@@ -68,8 +68,8 @@ program < in.txt 1> out.txt 2> error.txt
 
 # Pipes --------------------------------------------------
 
-| 					# makes standard output for a command the standard input for the next command
-					# wihtout creating intermediate file
+| 				# makes standard output for a command the standard input for the next command
+				# wihtout creating intermediate file
 
 # --------------------------------------------------------
 # Question: can we use the output of one program as the parameter of another program? 
